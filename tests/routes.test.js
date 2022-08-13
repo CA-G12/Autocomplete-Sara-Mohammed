847951ebabd403cd67a4a0aae8dc6b58551daba7
unknown <saritadh74@gmail.com>
2022-08-13 11:45:40 +0300

    added a test for the search route relates #7

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,41 +1,56 @@
 const router = require('../src/router');
-const supertest = require('supertest')
+const supertest = require('supertest');
 
-test('Test Home Routing', done => {
-    supertest(router).get('/')
-        .expect(200)
-        .expect('Content-Type', /html/)
-        .end((err, res) => {
-            if (err) {
-                done(err)
-            } else {
-                done()
-            }
-        })
-})
+test('Test Home Routing', (done) => {
+  supertest(router)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+});
 
-test('Test Css Routing', done => {
-    supertest(router).get('/public/style.css')
-        .expect(200)
-        .expect('Content-Type', /css/)
-        .end((err, res) => {
-            if (err) {
-                done(err)
-            } else {
-                done()
-            }
-        })
-})
+test('Test Css Routing', (done) => {
+  supertest(router)
+    .get('/public/style.css')
+    .expect(200)
+    .expect('Content-Type', /css/)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+});
 
-test('Test NotFound Routing', done => {
-    supertest(router).get('/dumyURL')
-        .expect(404)
-        .expect('Content-Type', /html/)
-        .end((err, res) => {
-            if (err) {
-                done(err)
-            } else {
-                done()
-            }
-        })
-})
+test('Test NotFound Routing', (done) => {
+  supertest(router)
+    .get('/dumyURL')
+    .expect(404)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+});
+
+test('Test search Route', (done) => {
+  supertest(router)
+    .get('/search/something')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) return done(err);
+      expect(res.statusCode).toBe(200);
+      done();
+    });
+});
